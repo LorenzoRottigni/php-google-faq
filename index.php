801cd368c954ce78dc8347bc81967cf824ad3567
LorenzoRottigni <lorenzo@rottigni.net>
2021-11-30 17:16:45 +0100

    Pagina completa

---
+++ index.php
@@ -0,0 +1,187 @@
+<?php
+    $HTMLContents = [
+        [
+            "title" => "<h2>How are you implementing the recent decision of the Court of Justice of the European Union (CJEU) on the right to be forgotten?</h2>",
+            "paragraphs" => [
+                "<p>
+                    The recent <a href='#'> decision by the Court of Justice of the European Union </a> has profound consequences for search engines in Europe. The court ruled
+                    that some users have the right to ask search engines such as Google to remove results for search keywords that include their name. In order
+                    to be removed, the displayed results must be inadequate, irrelevant or no longer relevant, or excessive.
+                </p>",
+
+                "<p>
+                    Since this decision was published on May 13, 2014, we have worked tirelessly to comply with it. This is a complex procedure because we have
+                    to evaluate every single request and make a balance between the individual's right to control their personal data and everyone's right 
+                    to know and distribute information.
+                </p>",
+
+                "<p>
+                    To submit a removal request, please fill out this <a href='#'> web form </a>. You will receive an automatic reply confirming the receipt of your request.
+                    We will then evaluate your case (please note that it may take some time as we have already received so many similar requests). In evaluating
+                    the request, we will consider whether the results include outdated information relating to your private life. We will also consider whether
+                    there is a public interest in the information remaining in our search results, for example if it relates to financial fraud, professional
+                    malpractice, criminal convictions or your public conduct in connection with a public office (elected or unelected). These assessments are
+                    complex, and as a private organization we may not be in the right position to make decisions about your case. If you disagree with our assessment,
+                </p>",
+
+                "<p>
+                    In the coming months, we will work closely with data protection authorities and other authorities to refine our approach. The CJEU's decision 
+                    represents a significant change for search engines. We are concerned about its consequences, but we also believe it is important to respect the
+                    Court's decision and are working to put in place a procedure that is compliant with the law. 
+                </p>",
+
+                "<p>
+                    When you search for a name, you may find a notification explaining that the results may have been changed to comply with European data protection
+                    laws. We show this notification to European users when they search for most names, not just pages that have been subject to a removal.
+                </p>"
+            ]
+        ],
+        [
+            "title" => "<h2> How does Google protect my privacy and keep my information safe? </h2>",
+
+            "paragraphs" => [
+                "<p>
+                    We know that security and privacy are important to you and they are important to us too. It is imperative for us to offer high security and make sure 
+                    your information is safe and accessible when you need it.
+                </p>",
+
+                "<p>
+                    We are constantly working to ensure high security, protect your privacy and make Google even more effective and efficient for you. We spend hundreds
+                    of millions of dollars each year on security and employ world-renowned data security experts to keep your information safe. We've also developed
+                    easy-to-use security and privacy tools like Google Dashboard, 2-Step Verification, and Ad Settings. So, when it comes to the information you share with Google,
+                    you have maximum control.
+                </p>",
+
+                "<p> You can read more about online safety, including how to protect yourself and your family online, in the <a href='#'> Google Online Safety Center </a></p>",
+
+                "<p> <a href='#'> Find </a> out what measures we take to ensure the protection and security of your personal information, leaving you in control. <p>"
+            ]
+        ],
+        [
+            "title" => "<h2> Why is my account associated with a country? </h2>",
+
+            "paragraphs" => [],
+
+            "listItems" => [
+                "<li>
+                    The Google subsidiary that offers the services, processes your information and is responsible for complying with applicable privacy laws. Google generally offers its consumer services through one of the following two companies:
+                    <ul>
+                        <li>Google Ireland Limited, if users are resident in the European Economic Area (European Union countries, plus Iceland, Liechtenstein and Norway) or Switzerland.</li>
+                        <li>Google LLC, based in the United States, for the rest of the world.</li>
+                    </ul>
+                </li>",
+
+                "<li>
+                    The version of the terms that governs our relationship, which may vary according to local laws.
+                </li>"
+            ]
+        ],
+        [
+            "title" => "<h3>Establish the country associated with your account</h3>",
+
+            "paragraphs" => [
+                "<p>
+                    When you create a new Google Account, we associate it with a country based on where it was created. For accounts created at least a year ago, we use the country from which
+                    you usually access Google services, typically the services where you have spent the most time in the last year.
+                </p>",
+
+                "<p> Frequent travel does not usually affect the country associated with your account. If you move to another country, it may take about a year to update the country association. </p>",
+
+                "<p> 
+                    If the country associated with your account does not match your country of residence, the reason could be the difference between the country you work in and the country you reside in,
+                    installing a virtual private network (VPN) to mask your address IP or residence near a territorial boundary. <a href='#'> Contact us </a> if you think the country associated with your account is wrong.
+                </p>"
+            ]
+        ],
+        [
+            "title" => "<h2> How do I remove information about myself from Google search results? </h2>",
+
+            "paragraphs" => [
+                "<p>
+                    Google search results mirror content that is publicly available on the web. Search engines can't remove content directly from websites, so removing search results from Google doesn't
+                    remove content from the web. If you want to remove something from the web. , you must <a href='#'> contact the webmaster </a> of the site on which the content is published and ask him to make a change.
+                    Also, if, under European data protection laws, you would like to request the removal of certain information about you displayed in Google search results, please <a href='#'> click here </a>. Once the content
+                    has been removed and Google has detected the update, the information will no longer appear in Google's search results. If you have an urgent removal request, you can also <a href='#'> visit our support page for more information </a>.
+                <p>"
+
+            ]
+        ],
+        [
+            "title" => "<h2> When I click on Google Search results, are my search keywords sent to websites? </h2>",
+
+            "paragraphs" => [
+                "<p>
+                    In some cases, yes. When you click on a Google Search result, your web browser may also redirect the Internet address, or URL, of the search results page to the target web page in the form of a referrer URL . Sometimes,
+                    the <a href='#'> URL of the search </a> results page may contain the search query you entered. If you use SSL search (Google's encrypted search feature), in most cases the search terms are not sent as part of the URL in referrer URLs. This
+                    behavior can be an exception, for example if you use some less popular browsers. More information on SSL research can be found <a href='#'>here</a>. Search queries or information contained in the referrer URL may be available through 
+                    oogle Analytics or an Application Programming Interface (API). Additionally, advertisers may receive information relating to the exact keywords that triggered the click on an ad.
+                </p>"
+            ]
+        ]
+    ];
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <header class="px-3">
+        <div class="navbar-brand d-flex gap-3">
+            <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="google logo">
+            <h3>Privacy and Terms</h3>
+        </div>
+        <nav>
+            <ul class="list-unstyled d-flex gap-3 mb-0">
+                <li>Introduction</li>
+                <li>Privacy Policy</li>
+                <li>Term of Service</li>
+                <li>Technologies</li>
+                <li class="active">Frequent Questions</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <?php
+            foreach ($HTMLContents as $HTMLElements => $HTMLElement) {
+                echo $HTMLElement["title"];
+                foreach ($HTMLElement["paragraphs"] as $paragraph => $textContent) {
+                    echo $textContent;
+                }
+                if(key_exists("listItems", $HTMLElement)){
+                    echo "<p> Please note that Google services are basically the same no matter which affiliate company offers them or the country your account is associated with. </p>";
+                    echo "<ul>";
+                    foreach ($HTMLElement["listItems"] as $listItems => $listItem) {
+                        echo $listItem;
+                    }
+                    echo "</ul>";
+                    echo "<p> Your account is associated with a country (or territory) in the Terms of Service in order to establish two things: <p>";
+                }else{
+                    continue;
+                }
+            }
+        
+        ?>
+        </main>
+    
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
